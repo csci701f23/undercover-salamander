5a9d7c2d72e89635e4e7b33ee6d48cf95923b14a
lh280 <133035427+lh280@users.noreply.github.com>
2023-10-18 20:28:47 -0400

    Plot html connection (#8)
    * plot-html
    
    pushing code and plot for image/html connection
    
    * Update index.html
    
    added map section
    
    * Update index.html
    
    updated map class
    
    * Update style.css
    
    added .map .climatemap
    
    * added data-map-exploration file
    
    added data-map-exploration temporarily to basic-website folder, for access to index.html
    
    * readd climate-6 photo
    
    for some reason climate-6 was replaced by an icloud version, this fixes that
    
    * Delete basic-website/images/.climate-6.png.icloud
    
    deletes extraneous file
    
    ---------
    
    Co-authored-by: cece ziegler <cziegler@middlebury.edu>

---
+++ data-map-exploration/plot1-html.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+# Load the data (replace with your file path)
+df = pd.read_csv('/Users/ceceziegler/Downloads/daily_rainfall_flags_removed.csv', parse_dates=['date'])
+
+# Filter the data to include only the year 2010
+df_2010 = df[df['date'].dt.year == 2010]
+
+# Group and sum the rainfall data
+grouped = df_2010.groupby(['state_code', 'date'])['rainfall'].sum().reset_index()
+
+# Load the GeoDataFrame (replace with your file path)
+gdf = gpd.read_file("/Users/ceceziegler/Downloads/gz_2010_us_040_00_500k.json")
+
+# Convert 'state_code' to an object type in the grouped DataFrame
+grouped['state_code'] = grouped['state_code'].astype(str)
+
+# Merge the GeoDataFrame with the grouped data
+merged = gdf.merge(grouped, left_on='STATE', right_on='state_code', how='left')
+
+# Create a spatial index for the GeoDataFrame
+merged.sindex
+
+# Define your custom vmin and vmax values
+vmin = 0  # Minimum value for the color scale
+vmax = 50  # Maximum value for the color scale
+
+# Create a figure with lower resolution
+fig, ax = plt.subplots(1, figsize=(8, 5), dpi=100)
+
+# Plot the GeoDataFrame with custom color scale
+merged.plot(column='rainfall', cmap='viridis', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True, vmin=vmin, vmax=vmax)
+
+# Customize the plot
+ax.set_title('Rainfall Choropleth Map for 2010', fontsize=16)
+ax.set_axis_off()  # Turn off the axis
+
+# save plot for html
+plt.savefig('rainfall.png')
+
+# Show the plot
+plt.show()
