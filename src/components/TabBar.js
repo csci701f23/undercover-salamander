343cff75aa20445ca36773f7b4d3f2518e060667
lh280 <133035427+lh280@users.noreply.github.com>
2023-11-16 21:00:50 -0500

    Adding tabs component (#27)
    Adding mostly working tabs: slight issue in styling, but it should be easily fixable.

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -1,9 +1,32 @@
 // might not need currentTab as a property
 export default function TabBar({currentTab, setCurrentTab}) {
+    const setTab = (tab) => {
+        setCurrentTab(tab);
+    }
+    
     return (
         <div>
-            <button onClick={() => setCurrentTab("PRCP")}>PRCP</button>
-            {/* button for each parameter */}
+            <div className="tabs-buttons">
+                <button className={currentTab === "PRCP" ? "tabs-buttons active-tabs" : "tabs-buttons"} onClick={() => setTab("PRCP")}>Precipitation</button>
+                <button className={currentTab === "SNOW" ? "tabs-buttons active-tabs" : "tabs-buttons"} onClick={() => setTab("SNOW")}>Snowfall</button>
+                <button className={currentTab === "MAXT" ? "tabs-buttons active-tabs" : "tabs-buttons"} onClick={() => setTab("MAXT")}>Maximum Temperature</button>
+                <button className={currentTab === "MINT" ? "tabs-buttons active-tabs" : "tabs-buttons"} onClick={() => setTab("MINT")}>Minimum Temperature</button>
+            </div>
+
+            <div className="tabs-content">
+                <div className={currentTab === "PRCP" ? "tabs-content  active-content" : "tabs-content"}>
+                    <h3>Precipitation</h3>
+                </div>
+                <div className={currentTab === "SNOW" ? "tabs-content  active-content" : "tabs-content"}>
+                    <h3>Snowfall</h3>
+                </div>
+                <div className={currentTab === "MAXT" ? "tabs-content  active-content" : "tabs-content"}>
+                    <h3>Maximum Temperature</h3>
+                </div>
+                <div className={currentTab === "MINT" ? "tabs-content  active-content" : "tabs-content"}>
+                    <h3>Minimum Temperature</h3>
+                </div>
+            </div>
         </div>
     )
 }
