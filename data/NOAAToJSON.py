239fef09acf68d6b498b21a97cb462c04bf38268
Jeff Blake <jtblake@middlebury.edu>
2023-11-19 16:41:34 -0500

    Parallelized the county portion as well, cleaned up again, might try to run on ada?

--- data/NOAAToJSON.py
+++ data/NOAAToJSON.py
@@ -1,16 +1,7 @@
-import numpy as np
 import pandas as pd
 import json
-import geopandas as gpd
-import geopy
-import geocoder
-from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
-import multiprocessing
 import requests
-# from multiprocessing import Process 
-# import threading
-# import parallelTestModule
+import multiprocessing
 
 placeholderColumns = ["ID", "Name", "County", "Latitude", "Longitude", "YearsArray", "YearsValues"]
 
@@ -21,10 +12,6 @@ stationInformation = pd.read_fwf("https://www1.ncdc.noaa.gov/pub/data/ghcn/daily
 colspecs = [(0,11), (11,15), (15,17), (17,21)]
 colnames = ["ID", "YEAR", "MONTH", "ELEMENT"]
 count =  1
-
-
-# https://geopy.readthedocs.io/en/stable/
-geolocator = Nominatim(user_agent="Undercover Salamander")
 
 # only adds daily entries, ignores the flags that exist in the dataset
 for i in range(21, 264, 8):
@@ -68,13 +55,11 @@ def processStation(id, name, latitude, longitude, param):
         # adapted from https://gis.stackexchange.com/questions/372872/max-retries-exceeded-with-url-in-nominatim-with-geopy try using requests instead of geopy
         location = requests.get(url=f'https://nominatim.openstreetmap.org/reverse?lat={latitude}&lon={longitude}&format=json&accept-language=en').json()
     except:
-        print("Nom error")
+        print("Nominatim error")
         return None
     
     county = location['address']['county'].replace(" County", "")
     state = location['address']['state']
-
-    print(county)
 
     if (county == None):
         print("county error")
@@ -82,7 +67,7 @@ def processStation(id, name, latitude, longitude, param):
 
     return [id, name, county, state, latitude, longitude, yearsSeries, yearlyVal]
 
-def processChunk(chunk):
+def processStationChunk(chunk):
     result = pd.DataFrame(columns=["ID", "NAME", "COUNTY", "STATE", "LAT", "LONG", "YEARS", "VALS"])
     param = "PRCP"
     for (key, value) in chunk.iterrows():
@@ -91,36 +76,49 @@ def processChunk(chunk):
             result.loc[len(result)] = row
     return result
 
+def processCountyChunk(chunk):
+    return chunk.groupby(["COUNTY", "STATE"]).apply(averageCounty)
+
 def processAllStations(param):
     stateStations = stationInformation.loc[stationInformation["ID"].str.contains("US")]
     # adapted parallelization start from: https://stackoverflow.com/questions/40357434/pandas-df-iterrows-parallelization
     num_processes = multiprocessing.cpu_count()
     length = stateStations.shape[0]
 
-    chunk_size = int((length + 1)/num_processes)
+    chunk_size = int((length + num_processes - 1)/num_processes)
 
     chunks = []
 
     for i in range(0, length, chunk_size):
         chunks.append(stateStations.iloc[i:min(i + chunk_size, length)])
     
-    # TODO: Make sure param is being passed through into processChunk
+    # TODO: Make sure param is being passed through into processChunk programatically
     if __name__ == '__main__':    
         pool = multiprocessing.Pool(processes=num_processes)
         
-        results = pool.map(processChunk, chunks)
+        results = pool.map(processStationChunk, chunks)
         
         pool.close()
         pool.join()
         
-        stateFrame = pd.concat(results).reset_index(drop=True)
-
-        for (key, value) in stateStations.iterrows():
-            row = processStation(value["ID"], value["Name"], value["Latitude"], value["Longitude"], param)
-            if (row is not None):
-                stateFrame.loc[len(stateFrame)] = row
+        stationFrame = pd.concat(results).reset_index(drop=True)
         
-        countyData = stateFrame.groupby(["COUNTY", "STATE"]).apply(averageCounty)
+        # Redefining chunk size here in case some stations weren't processed
+        stationFrameLength = stationFrame.shape[0]
+        stationFrame_chunk_size = int((stationFrame.shape[0] + num_processes - 1)/num_processes)
+        stationFrame_chunks = []
+
+        for i in range(0, stationFrameLength, stationFrame_chunk_size):
+            stationFrame_chunks.append(stationFrame.iloc[i:min(i + stationFrame_chunk_size, stationFrameLength)])
+
+        # Is there a way to have parallel -> serial -> parallel blocks without having to create a new pool with multiprocessing?
+        countyPool = multiprocessing.Pool(processes=num_processes)
+        county_results = countyPool.map(processCountyChunk, stationFrame_chunks)
+
+        countyPool.close()
+        countyPool.join()
+
+        countyData = pd.concat(county_results)
         
         with open("./data/PRCP_info.json", "w+") as f:
             countyData.to_json(f, orient="table", indent=4)
