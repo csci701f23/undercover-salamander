9b48c11b17aa5be274f7fbffd86962f5b3defe7c
cece ziegler <cziegler@middlebury.edu>
2023-12-01 13:17:10 -0500

    adding text for values

--- src/components/Scale.js
+++ src/components/Scale.js
@@ -10,16 +10,16 @@ const Scale = ({ currentTab }) => {
 
     if (currentTab === 'min temp') {
       newScaleGradient = 'linear-gradient(to bottom, #C2FCF8, #88D8DA, #41ADC5, #1692B6)';
-      newKeyValues = [0, 25, 50, 100];
+      newKeyValues = ['0', '25', '50', '100', 'Daily Avg (C)'];
     } else if (currentTab === 'max temp') {
       newScaleGradient = 'linear-gradient(to bottom, #FFFFAD, #FFCB58, #F68F50, #E0603F)';
-      newKeyValues = [0, 25, 50, 100];
+      newKeyValues = ['0', '25', '50', '100', 'Daily Avg (C)'];
     } else if (currentTab === 'PRCP') {
       newScaleGradient = 'linear-gradient(to bottom, #E5FAC0, #B4E197, #83BD75, #4E944F)';
       newKeyValues = ['0', '25', '50', '100', 'Daily Avg (mm)'];
     } else if (currentTab === 'snowfall') {
       newScaleGradient = 'linear-gradient(to bottom, #F0E3FF, #D5B7F7, #916DD5, #6235AA)';
-      newKeyValues = [0, 25, 50, 100];
+      newKeyValues = ['0', '25', '50', '100', 'Daily Avg (mm)'];
     }
 
     setScaleGradient(newScaleGradient);
