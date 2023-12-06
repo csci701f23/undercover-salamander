6ecda1d36d2184cfef92cfd7c69451bf16136866
lh280 <133035427+lh280@users.noreply.github.com>
2023-12-06 10:55:53 -0500

    adds deployment code (#48)
    * fix precipitation
    
    * add gh-pages
    
    * delete pages.yml
    
    * stops ignoring out file and adds pages.yml
    
    * changes path
    
    * adds next.config.js

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -7,7 +7,7 @@ export default function TabBar({currentTab, setCurrentTab}) {
     return (
         <div>
             <div className="tabs-buttons">
-                <button className={currentTab === "PRCP" ? "tabs-buttons active-tabs" : "tabs-buttons"} onClick={() => setTab("PRCP")}>Precipitation</button>
+                <button className={currentTab === "PRCP" ? "tabs-buttons active-tabs" : "tabs-buttons"} onClick={() => setTab("PRCP")}>Rainfall</button>
                 <button className={currentTab === "SNOW" ? "tabs-buttons active-tabs" : "tabs-buttons"} onClick={() => setTab("SNOW")}>Snowfall</button>
                 <button className={currentTab === "MAXT" ? "tabs-buttons active-tabs" : "tabs-buttons"} onClick={() => setTab("MAXT")}>Maximum Temperature</button>
                 <button className={currentTab === "MINT" ? "tabs-buttons active-tabs" : "tabs-buttons"} onClick={() => setTab("MINT")}>Minimum Temperature</button>
@@ -15,7 +15,7 @@ export default function TabBar({currentTab, setCurrentTab}) {
 
             <div className="tabs-content">
                 <div className={currentTab === "PRCP" ? "tabs-content  active-content" : "tabs-content  inactive"}>
-                    <h3>Precipitation</h3>
+                    <h3>Rainfall</h3>
                 </div>
                 <div className={currentTab === "SNOW" ? "tabs-content  active-content" : "tabs-content  inactive"}>
                     <h3>Snowfall</h3>
