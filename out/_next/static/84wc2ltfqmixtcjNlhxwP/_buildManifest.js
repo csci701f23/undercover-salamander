c3cd535fa7c7ad416d850c16953a4749bd39d60a
Jeff Blake <jtblake@middlebury.edu>
2023-12-16 16:07:24 -0500

    Finished and build and fixed scale

--- out/_next/static/84wc2ltfqmixtcjNlhxwP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/645-4a5dc62d57d811ec.js","static/chunks/pages/index-8852bca4a7eac7a7.js"],"/_error":["static/chunks/pages/_error-8b31ab893441781f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
