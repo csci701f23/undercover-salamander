0e1c1f064c9bfa5175c481e5f852eab512c92df8
Jeff Blake <jtblake@middlebury.edu>
2023-12-15 19:40:22 -0500

    Export

--- src/components/Map.js
+++ src/components/Map.js
@@ -15,29 +15,24 @@ export default function Map ({ year, width, height, geoData, currentTab }) {
   const getData = (tab) => {
     switch (currentTab) {
       case 'PRCP':
-        // numData = prcpData;
         return [prcpData, d3.scaleLinear()
-          .domain([0, 10, 20, 30, 50])
+          .domain([0, 1, 2, 3, 5])
           .range(["#E5fAC0", "#B4E197", "#83BD75", "#4E944F", "#2A6D2B"]), 10, "mm"];
       case 'SNOW':
-        // numData = prcpData; // placeholder
         return [snowData, d3.scaleLinear()
-          .domain([0, 1, 2, 3, 4])
+          .domain([0, 0.25, 0.5, 0.75, 1])
           .range(["#F0E3FF", "#D5B7F7", "#916DD5", "#7346BB", "#592BA2"]), 10, "mm"];
       case 'MAXT':
-        // numData = maxtData;
         return [maxtData, d3.scaleLinear()
-          .domain([0, 10, 20, 30, 40])
+          .domain([27.5, 33.75, 37.5, 41.25, 45])
           .range(["#FFFFAD", "#FFCB58", "#F68F50", "#E0603F", "#BE3613"]), 10, "°C"];
       case 'MINT':
-        // numData = prcpData; // placeholder
         return [prcpData, d3.scaleLinear()
           .domain([0, 10, 20, 30, 50])
           .range(["#C2FCF8", "#88D8DA", "#41ADC5", "#1692B6", "#055D96"]), 10, "°C"];
       default:
-        // numData = prcpData;
         return [prcpData, d3.scaleLinear()
-          .domain([0, 10, 20, 30, 50])
+          .domain([0, 1, 2, 3, 5])
           .range(["#E5fAC0", "#B4E197", "#83BD75", "#4E944F", "#2A6D2B"]), 10, "mm"];
       };
   }
